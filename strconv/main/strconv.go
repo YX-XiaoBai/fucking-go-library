1793888e73f758764705c0453f1c93547863459d
chenronghua <4650032992@qq.com>
2020-11-25 11:00:25 +0800

    feat: add new package

---
+++ strconv/main/strconv.go
@@ -0,0 +1,56 @@
+package main
+
+func main() {
+
+	// Atoi
+	//s1 := "100"
+	//i1, err := strconv.Atoi(s1)
+	//if err != nil {
+	//	fmt.Println("covert to int fail")
+	//}
+	//fmt.Printf("type:%T value:%#v\n", i1, i1)
+
+	// Itoa
+	//i2 := 200
+	//s2:= strconv.Itoa(i2)
+	//fmt.Printf("type:%T value:%#v\n", s2, s2)
+
+	// example for ParseXx()
+	//b, err := strconv.ParseBool("true")
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//fmt.Printf("type:%T value:%#v\n", b, b)
+	//f, err1 := strconv.ParseFloat("3.1415", 64)
+	//if err != nil {
+	//	fmt.Println(err1)
+	//}
+	//fmt.Printf("type:%T value:%#v\n", f, f)
+	//i, err2 := strconv.ParseInt("-2", 10, 64)
+	//if err2 != nil {
+	//	fmt.Println(err2)
+	//}
+	//fmt.Printf("type:%T value:%#v\n", i, i)
+	//u, err3 := strconv.ParseUint("2", 10, 64)
+	//if err3 != nil {
+	//	fmt.Println(err3)
+	//}
+	//fmt.Printf("type:%T value:%#v\n", u, u)
+
+	// example for FormatXx()
+	//s1 := strconv.FormatBool(true)
+	//s2 := strconv.FormatFloat(3.1415, 'E', -1, 64)
+	//s3 := strconv.FormatInt(-2, 16)
+	//s4 := strconv.FormatUint(2, 16)
+	//fmt.Printf("s1:%#v s2:%#v s3:%#v s4:%#v", s1, s2, s3, s4)
+
+	// IsPrint()
+	//s := []rune("小白")
+	//ss1 := strconv.IsPrint(s[0])
+	//fmt.Println(ss1)
+
+	// CanBackquote()
+ 	//s2 := "xiao bai"
+ 	//ss2 := strconv.CanBackquote(s2)
+	//fmt.Println(ss2)
+}
